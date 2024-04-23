0c33229b36313d2698d074e92f2c64e7b0948afe
Diksha125 <148642679+Diksha125@users.noreply.github.com>
2024-04-24 01:30:47 +0530

    Update variables_screen.dart

--- lib/view/variables_screen.dart
+++ lib/view/variables_screen.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:stock_scan_task/model/model.dart';
 
-class VariableScreen extends StatefulWidget {
+class VariableScreen extends StatelessWidget {
   final List<Criteria> criteria;
-  const VariableScreen({super.key, required this.criteria});
 
-  @override
-  State<VariableScreen> createState() => _VariableScreenState();
-}
+  VariableScreen({super.key, required this.criteria});
 
-class _VariableScreenState extends State<VariableScreen> {
+  final list = [-3, -1, -2, -5, -10];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,44 +18,23 @@ class _VariableScreenState extends State<VariableScreen> {
           color: Theme.of(context).colorScheme.onBackground,
           padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 25),
           child: ListView.builder(
-            itemCount: widget.criteria.length,
-            itemBuilder: (context, index) {
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  for (var criteria in widget.criteria)
-                    ListTile(
-                      title: criteria.variable!.isNotEmpty
-                          ? Expanded(
-                            child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: criteria.variable!.keys.map((key) {
-                                  return ListView.builder(
-                                      itemCount: criteria.variable![key][index],
-                                      itemBuilder: (context, index) {
-                                        return Expanded(
-                                          child: Column(
-                                            children: [
-                                              Text(
-                                                '$key: ${criteria.variable![key]}',
-                                                style: const TextStyle(
-                                                  fontSize: 10.0,
-                                                  color: Colors.white,
-                                                ),
-                                              ),
-                                              const Divider(),
-                                            ],
-                                          ),
-                                        );
-                                      });
-                                }).toList(),
-                              ),
-                          )
-                          : null,
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: list.length,
+              itemBuilder: (ctx, innerIndex) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      list[innerIndex].toString(),
+                      style: const TextStyle(
+                        color: Colors.white,
+                      ),
                     ),
-                ],
-              );
-            },
+                    const Divider()
+                  ],
+                );
+              }
           ),
         ),
       ),
